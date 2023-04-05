d0afd4833c63be4a90446f0942418c44cd066010
Nicholas <nporter108@gmail.com>
2023-04-05 15:54:55 -0400

    Initial commit

---
+++ src/KantsNightmare/Philosopher.java
@@ -0,0 +1,49 @@
+package KantsNightmare;
+import java.util.concurrent.locks.*;
+
+public class Philosopher implements Runnable{
+
+    int philosopher;
+    //All philosophers have access to the same forks, but this is the resource to be shared over time
+    static Lock[] forks;
+    int meatballs;
+
+    public Philosopher(int n){
+        philosopher = n;
+        meatballs = 5;
+        forks = new Lock[5];
+        for(int i = 0; i < 5; i++){
+            forks[i] = new ReentrantLock();
+        }
+    }
+
+
+    @Override
+    public void run() {
+        while(meatballs > 0) {
+            //Fork for left hand correlates to the fork in array at out current philosopher number:
+            if(forks[philosopher].tryLock()) {
+                System.out.println(philosopher + " Using left fork " + philosopher);
+                //then plus one for right hand fork
+                if (forks[(philosopher + 1) % 5].tryLock()) {
+                    System.out.println("Using right fork " + (philosopher + 1) % 5);
+                    //eata da meatball
+                    meatballs--;
+                    System.out.println("Philosopher " + philosopher + " has " + meatballs + " meatballs left...");
+                    //relinquish the fork
+                    //System.out.println("Relinquishing right fork " + (philosopher + 1) % 5);
+                    forks[(philosopher + 1) % 5].unlock();
+                }
+                System.out.println("Relinquish left fork " + philosopher);
+                forks[philosopher].unlock();
+            }
+            //Existential moaning
+            try {
+                Thread.sleep(10);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+    }
+}
